6260a9309f6c1d974eb2f1e8de05d465dde8dac9
six-ju <82314404+six-ju@users.noreply.github.com>
2024-07-01 19:35:03 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 47.19 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+    let a = Number(input[0])
+    let b = Number(input[1])
+    console.log( a +  " + " + b + " = " + (a+b) )
+})
